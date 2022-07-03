5d656ccd86cb4a1e994a1a877e6000f41ff9713d
Mel <meljohnpualon@gmail.com>
2022-07-03 10:35:46 +0800

    added setp component for slider

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const burgerMenu = document.querySelector('.btn-menu');
+const slider = document.querySelector('.slider');
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
 
 burgerMenu.addEventListener('click', () => {
     console.log('menu is clicked')
